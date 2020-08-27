d8ebb43ff8e46a428ee7fd2c9e612023cf68361c
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-27 14:18:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
-
-  if (right < 0) {
-    didger.style.right = `${right + 1}px`;
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  
+  if (left > 0) {
+    dodger.style.left = `${left + 1}px`
   }
 }
